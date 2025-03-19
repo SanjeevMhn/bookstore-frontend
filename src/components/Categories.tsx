e17bddb51080e607807e0d90c66be68e507e1e90
SanjeevMhn <sanjeevmaharjan19@gmail.com>
2025-03-19 17:26:57 +0545

    [FIXED]: - fixed book card css if background is not white in homepage - fixed only open the sidenav category dropdown if the sub category is   active

--- src/components/Categories.tsx
+++ src/components/Categories.tsx
@@ -73,42 +73,52 @@ const Categories = ({ activeCategory }: CategoriesProp) => {
         {
           id: 21,
           name: "Biography/Autobiography/Memoir",
+          route: "biography-autobiography-memoir",
         },
         {
           id: 22,
           name: "Self-Help",
+          route: "self-help",
         },
         {
           id: 23,
           name: "Health & Wellness",
+          route: "health-wellness",
         },
         {
           id: 24,
           name: "Cooking / Food",
+          route: "cooking-food",
         },
         {
           id: 24,
           name: "Travel",
+          route: "travel",
         },
         {
           id: 25,
           name: "History",
+          route: "history",
         },
         {
           id: 26,
           name: "Politics",
+          route: "politics",
         },
         {
           id: 27,
           name: "True Crime",
+          route: "true-crime",
         },
         {
           id: 28,
           name: "Psychology",
+          route: "psychology",
         },
         {
           id: 29,
           name: "Philosophy",
+          route: "philosophy",
         },
       ],
     },
@@ -120,44 +130,54 @@ const Categories = ({ activeCategory }: CategoriesProp) => {
         {
           id: 31,
           name: "Picture Books",
+          route: "picture-books",
         },
         {
           id: 32,
           name: "Coloring Books",
+          route: "coloring-books",
         },
         {
           id: 33,
           name: "Young Adult",
+          route: "young-adult",
         },
       ],
     },
     {
       id: 4,
       name: "Poetry",
+      route: "poetry",
     },
     {
       id: 5,
       name: "Graphic Novels/Comics",
+      route: "graphic-novels-comics",
     },
     {
       id: 6,
       name: "Art & Photography",
+      route: "art-photography",
     },
     {
       id: 7,
       name: "Science & Nature",
+      route: "science-nature",
     },
     {
       id: 8,
       name: "Business & Finance",
+      route: "business-finance",
     },
     {
       id: 9,
       name: "Religion & Spirituality",
+      route: "religion-spirituality",
     },
     {
       id: 10,
       name: "Sports & Outdoors",
+      route: "sports-outdoors",
     },
     {
       id: 11,
@@ -166,18 +186,34 @@ const Categories = ({ activeCategory }: CategoriesProp) => {
         {
           id: 111,
           name: "Dictionaries",
+          route: "dictionaries",
         },
         {
           id: 112,
           name: "Encyclopedias",
+          route: "encyclopedias",
         },
         {
           id: 113,
           name: "Atlases",
+          route: "atlases",
         },
       ],
     },
   ];
+
+  const hasSubCategory = (parent: Category, activeCat: string): boolean => {
+    if (activeCat) {
+      let parentSubCats = categories.find(
+        (cat) => cat.id == parent.id
+      )?.sub_category;
+      if (parentSubCats) {
+        return parentSubCats.some((cat) => cat.route == activeCat);
+      }
+      return false;
+    }
+    return false;
+  };
   return (
     <section className="category-list-container h-full">
       <ul className="category-list flex flex-col gap-[1rem] h-full overflow-y-auto bg-slate-200">
@@ -188,23 +224,16 @@ const Categories = ({ activeCategory }: CategoriesProp) => {
                 className="category-item text-[1.8rem]"
                 key={category.id + index}
               >
-                <details open>
-                  <summary>
-                    {category.name}
-                  </summary>
+                <details open={hasSubCategory(category, activeCategory!)}>
+                  <summary>{category.name}</summary>
                   <ul className="category-sub-list pl-8">
                     {category.sub_category?.map((sub, index) => (
-                      <li
-                        className="category-item"
-                        key={category.id + index}
-                      >
+                      <li className="category-item" key={category.id + index}>
                         {sub.route !== undefined ? (
                           <Link
                             href={`/books/genres/${sub.route}`}
                             className={`flex category-link ${
-                              activeCategory == sub.route
-                                ? "active"
-                                : ""
+                              activeCategory == sub.route ? "active" : ""
                             }`}
                           >
                             {sub.name}
@@ -227,11 +256,26 @@ const Categories = ({ activeCategory }: CategoriesProp) => {
             );
           }
           return (
-            <li
-              className="category-item text-[1.8rem] cursor-pointer hover:bg-blue-300 p-[0.8rem_1.5rem] rounded-md"
-              key={category.id + index}
-            >
-              {category.name}
+            <li className="category-item" key={category.id + index}>
+              {category.route !== undefined ? (
+                <Link
+                  href={`/books/genres/${category.route}`}
+                  className={`flex category-link ${
+                    activeCategory == category.route ? "active" : ""
+                  }`}
+                >
+                  {category.name}
+                </Link>
+              ) : (
+                <>
+                  <Link
+                    href={`/books/genres/${category.name}`}
+                    className="flex category-link"
+                  >
+                    {category.name}
+                  </Link>
+                </>
+              )}
             </li>
           );
         })}
