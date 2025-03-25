8fe8c6ab3d304c04d8443ad7ae67854e6931d45c
SanjeevMhn <sanjeevmaharjan19@gmail.com>
2025-03-25 18:05:48 +0545

    [ADDED]: added Ctrl + k shortcut to open Search form

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -1,15 +1,26 @@
 "use client";
-import { FC, useState } from "react";
+import { FC, useEffect, useState } from "react";
 import Footer from "./Footer";
 import Navbar from "./Navbar";
 import { useOpenModal } from "@/providers/OpenModalContext";
 
 const MainLayout: FC<{ children: React.ReactNode }> = ({ children }) => {
-  const { openModal } = useOpenModal();
+  useEffect(() => {
+    const handleKeydown = (e: KeyboardEvent) => {
+      if ((e.ctrlKey || e.metaKey) && e.key.toLowerCase() === "k") {
+        e.preventDefault();
+        handleOpenModal("Full Search");
+      }
+    };
+    document.addEventListener("keydown", handleKeydown);
+    return () => {
+      document.removeEventListener("keydown", handleKeydown);
+    };
+  }, []);
+  const { openModal, handleOpenModal } = useOpenModal();
   return (
     <main
-      className={
-        `flex flex-col h-full w-full isolate 
+      className={`flex flex-col h-full w-full isolate 
         ${openModal !== null ? "fixed" : ""} 
       `}
     >
